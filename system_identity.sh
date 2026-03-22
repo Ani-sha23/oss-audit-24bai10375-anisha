49ed3f07d6ed10f790d8c5b77ea4a0381e6ceedf
Anisha Garg <anishagarg2806@gmail.com>
2026-03-22 18:40:54 +0530

    Update system_identity.sh

--- system_identity.sh
+++ system_identity.sh
@@ -1,1 +1,25 @@
+#!/bin/bash
+# Script 1: System Identity Report
+# Author: Anisha Garg
 
+STUDENT_NAME="Anisha Garg"
+SOFTWARE="Linux Kernel"
+
+KERNEL=$(uname -r)
+USER=$(whoami)
+HOME_DIR=$HOME
+UPTIME=$(uptime -p)
+DATE=$(date)
+DISTRO=$(grep "^PRETTY_NAME=" /etc/os-release | cut -d= -f2 | tr -d '"')
+
+echo "======================================"
+echo " Open Source Audit — $STUDENT_NAME"
+echo "======================================"
+echo "Software       : $SOFTWARE"
+echo "Distribution   : $DISTRO"
+echo "Kernel Version : $KERNEL"
+echo "User           : $USER"
+echo "Home Directory : $HOME_DIR"
+echo "Uptime         : $UPTIME"
+echo "Date & Time    : $DATE"
+echo "License        : GPL v2 (Open Source)"
