21d5c2602f532f63fa7b00ede43ca39d2120eb65
Anisha Garg <anishagarg2806@gmail.com>
2026-03-22 19:20:30 +0530

    Rename USER variable to USER_NAME and update its usage

--- system_identity.sh
+++ system_identity.sh
@@ -6,11 +6,11 @@ STUDENT_NAME="Anisha Garg"
 SOFTWARE="Linux Kernel"
 
 KERNEL=$(uname -r)
-USER=$(whoami)
+USER_NAME=$(whoami)
 HOME_DIR=$HOME
 UPTIME=$(uptime -p)
 DATE=$(date)
-DISTRO=$(grep "^PRETTY_NAME=" /etc/os-release | cut -d= -f2 | tr -d '"')
+DISTRO=$(uname -o)
 
 echo "======================================"
 echo " Open Source Audit — $STUDENT_NAME"
@@ -18,7 +18,7 @@ echo "======================================"
 echo "Software       : $SOFTWARE"
 echo "Distribution   : $DISTRO"
 echo "Kernel Version : $KERNEL"
-echo "User           : $USER"
+echo "User           : $USER_NAME"
 echo "Home Directory : $HOME_DIR"
 echo "Uptime         : $UPTIME"
 echo "Date & Time    : $DATE"
