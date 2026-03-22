c507ad24eac660e1af0498b09e4e4f3ced99d91c
Anisha Garg <anishagarg2806@gmail.com>
2026-03-22 18:41:25 +0530

    Add package inspector script
    Added a script to check if a specific Linux package is installed and provide information about it.

--- package_inspector.sh
+++ package_inspector.sh
@@ -1,1 +1,26 @@
+#!/bin/bash
+# Script 2: Package Inspector
 
+PACKAGE="linux-image-$(uname -r)"
+
+if dpkg -l | grep -q $PACKAGE; then
+    echo "$PACKAGE is installed."
+    dpkg -l | grep $PACKAGE
+else
+    echo "$PACKAGE is NOT installed."
+fi
+
+case $PACKAGE in
+    linux-image*)
+        echo "Linux Kernel: Core of the operating system"
+        ;;
+    apache2)
+        echo "Apache: Web server powering the internet"
+        ;;
+    mysql)
+        echo "MySQL: Open-source database system"
+        ;;
+    *)
+        echo "Unknown package"
+        ;;
+esac
